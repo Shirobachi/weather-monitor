35f45ad5993f9f65b7530c0bf5ffc8849a4c31f1
Simon Hryszko <simon@hryszko.dev>
2021-07-05 15:46:23 +0200

    Added routes to login and register

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\authController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [authController::class, 'login']);
+Route::get('register', [authController::class, 'register']);
