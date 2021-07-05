b603665c407cac977c14c695fe90c7fec254edcb
Simon Hryszko <simon@hryszko.dev>
2021-07-05 16:06:40 +0200

    More route about autentification

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,12 @@ use App\Http\Controllers\authController;
 
 
 Route::get('/', [authController::class, 'login']);
+Route::post('/', [authController::class, 'tryLogin']);
+
 Route::get('register', [authController::class, 'register']);
+Route::post('register', [authController::class, 'signup']);
+
+Route::get('/logout', function () {
+  session()->forget('userID');
+  return redirect(url('/'));
+});
