a6fbc92b8c2dd6a76560da0597663c371b9a1727
Simon Hryszko <simon@hryszko.dev>
2021-07-08 15:13:23 +0200

    Added new colimn to weather_info

---
+++ database/migrations/2021_07_08_132341_create_weather_infos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTownIDToWeatherInfs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('weather_infos', function (Blueprint $table) {
+            $table->integer('townID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('weather_infos', function (Blueprint $table) {
+            $table->integer('townID');
+        });
+    }
+}
