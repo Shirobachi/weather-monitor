235f17878202b7797071f357ecd2992e7592d82c
Simon Hryszko <simon@hryszko.dev>
2021-07-08 16:39:36 +0200

    Added env variable WEATHER_API_KEY

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ Route::get('/logout', function () {
   session()->forget('userID');
   return redirect(url('/'));
 });
-
 Route::get('dashboard', [weatherController::class, 'dashboard']);
 Route::get('updateCities', [weatherController::class, 'updateCitiesShow']);
 Route::post('updateCities', [weatherController::class, 'updateCities']);
